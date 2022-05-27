0ea19c7f671ab8d3260fc648b1b6074c41f94baa
niyaz-aghazade <niyazagazade883@gmail.com>
2022-05-27 15:17:43 +0400

    slm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
+import { Provider } from "react-redux";
 import App from "./App";
-
+import store from "./redux/store";
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+  rootElement
+);
